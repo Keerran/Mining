3b1571210a79ffcf046ef8887cce170683f6838e
CHRISO1503 <christopher_oakes@hotmail.com>
2022-06-16 23:41:23 +0100

    Added camera collision

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Cam : MonoBehaviour
 {
     public float rotateSpeed = 5;
+    public float distToPlayer = 6;
 
     private Vector3 _offset;
+    private RaycastHit closeZoom;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +24,17 @@ public class Cam : MonoBehaviour
 
         transform.RotateAround(transform.parent.position, transform.parent.right, -vertical);
         transform.parent.Rotate(Vector3.up * horizontal);
+
+        var ray = new Ray(transform.parent.position, -transform.forward);
+        Debug.DrawRay(transform.parent.position, -transform.forward * distToPlayer, Color.magenta);
+        //disttoplayer
+        if (Physics.Raycast(ray, out closeZoom, distToPlayer, ~LayerMask.NameToLayer("Terrain")))
+        {
+            transform.position = transform.parent.position - transform.forward * (closeZoom.distance - 0.1f);
+        }
+        else
+        {
+            transform.position = transform.parent.position - transform.forward * (distToPlayer - 0.1f);
+        }
     }
 }
