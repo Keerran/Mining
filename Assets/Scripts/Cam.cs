6d62d17ef798bdb08387fa6a8480f4a8c6192805
Keerran <kierankumar@gmail.com>
2022-06-23 22:31:10 +0100

    Fix materials and prototype camera.

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -4,37 +4,48 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
+    public float maxAngle;
+    public float minAngle;
     public float rotateSpeed = 5;
     public float distToPlayer = 10;
     public Transform focalPoint;
+    public LayerMask layerMask;
 
-    private RaycastHit _closeZoom;
+    private float _xRotation, _yRotation = 10 * Mathf.Deg2Rad;
 
     // Start is called before the first frame update
     void Start()
     {
 
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed * 0.01f;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed * 0.01f;
 
-        transform.RotateAround(focalPoint.position, transform.parent.right, -vertical);
-        transform.parent.Rotate(Vector3.up * horizontal);
+        _yRotation += vertical;
+        _yRotation = Mathf.Clamp(_yRotation, minAngle * Mathf.Deg2Rad, maxAngle * Mathf.Deg2Rad);
+        _xRotation = (_xRotation - horizontal) % (2 * Mathf.PI);
+
+        var pos = new Vector3(
+            Mathf.Sin(_yRotation) * Mathf.Cos(_xRotation),
+            Mathf.Cos(_yRotation),
+            Mathf.Sin(_yRotation) * Mathf.Sin(_xRotation)
+        );
+
+        transform.position = pos + focalPoint.position;
+        transform.LookAt(focalPoint);
 
         var ray = new Ray(focalPoint.position, -transform.forward);
-        Debug.DrawRay(focalPoint.position, -transform.forward * distToPlayer, Color.magenta);
-        //disttoplayer
-        if (Physics.Raycast(ray, out _closeZoom, distToPlayer, ~LayerMask.NameToLayer("Terrain")))
+        var distance = distToPlayer;
+        if (Physics.Raycast(ray, out RaycastHit closeZoom, distToPlayer, layerMask))
         {
-            transform.position = focalPoint.position - transform.forward * (_closeZoom.distance - 0.1f);
+            distance = closeZoom.distance;
         }
-        else
-        {
-            transform.position = focalPoint.position - transform.forward * (distToPlayer - 0.1f);
-        }
+
+        transform.position = focalPoint.position - transform.forward * (distance - 0.1f);
     }
 }
