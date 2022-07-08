7499fc8acd7e0b3e8585fa5b0df45b40c4212173
CHRISO1503 <christopher_oakes@hotmail.com>
2022-07-08 19:54:29 +0100

    Added carnival scene

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -6,15 +6,15 @@ public class Cam : MonoBehaviour
 {
     public float maxAngle;
     public float minAngle;
-    public float rotateSpeed = 5;
     public float distToPlayer = 10;
     public LayerMask layerMask;
+    public float smoothLookTime;
+    public float smoothDistanceTime;
 
     private Controls _controls;
     private Transform _focalPoint;
     private Vector3 _currentLook;
     private Vector3 _lookVelocity;
-    private Vector3 _moveVelocity;
     private float _distVelocity;
     private Rigidbody _playerRb;
 
@@ -27,21 +27,23 @@ public class Cam : MonoBehaviour
         _playerRb = player.GetComponent<Rigidbody>();
         _controls = StateManager.controls;
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        var input = _controls.Player.Look.ReadValue<Vector2>() * rotateSpeed * 0.0005f;
+        var input = _controls.Player.Look.ReadValue<Vector2>() * 0.0025f;
 
-        if(GameState.instance.inputBlocked)
+        if (GameState.instance.inputBlocked)
             input = Vector2.zero;
 
-        if(input == Vector2.zero)
+        if (input == Vector2.zero)
         {
             transform.position -= _playerRb.velocity * Time.deltaTime;
         }
-        _currentLook = Vector3.SmoothDamp(_currentLook, _focalPoint.position, ref _lookVelocity, 0.3f);
+        _currentLook = Vector3.SmoothDamp(_currentLook, _focalPoint.position, ref _lookVelocity, smoothLookTime);
+
+        // Camera position relative to current look in spherical coordinates
         var pos = transform.position - _currentLook;
         var r = pos.magnitude;
         var theta = Mathf.Acos(pos.y / r);
@@ -50,7 +52,8 @@ public class Cam : MonoBehaviour
         theta = Mathf.Clamp(theta + input.y, minAngle * Mathf.Deg2Rad, maxAngle * Mathf.Deg2Rad);
         phi -= input.x;
 
-        var distance = Mathf.SmoothDamp(r, distToPlayer, ref _distVelocity, 0.1f);
+        var distance = Mathf.SmoothDamp(r, distToPlayer, ref _distVelocity, smoothDistanceTime);
+        // var distance = distToPlayer;
 
         pos = distance * new Vector3(
             Mathf.Sin(theta) * Mathf.Cos(phi),
@@ -59,10 +62,12 @@ public class Cam : MonoBehaviour
         );
 
         var targetPos = pos + _currentLook;
-        var ray = new Ray(_currentLook, pos);
-        if(Physics.Raycast(ray, out var hit, pos.magnitude, layerMask))
-            targetPos = hit.point;
         
+        // Move in front terrain between player and camera
+        var ray = new Ray(_currentLook, pos);
+        if (Physics.Raycast(ray, out var hit, pos.magnitude, layerMask))
+            targetPos = hit.point;
+
         transform.position = targetPos;
         transform.LookAt(_currentLook);
     }
